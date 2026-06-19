270947c11f100ae315dd4d625d5b8c2aef37dfbc
Simon Lauper <simon.lauper2008@gmail.com>
2026-06-19 16:50:33 +0200

    init state

---
+++ android/app/src/main/kotlin/com/example/pokemon_client/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pokemon_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon_client/src/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
